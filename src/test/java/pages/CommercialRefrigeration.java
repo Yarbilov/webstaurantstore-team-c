d0e6085905232f49818ec3ca32d10d72907c2a4e
alex.tokmin <atokmin@gmail.com>
2023-03-24 12:38:09 -0400

    Commercial Refregeration. Alex's code

---
+++ src/test/java/pages/CommercialRefrigeration.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+public class CommercialRefrigeration {
+
+        public CommercialRefrigeration(){
+            PageFactory.initElements(Driver.getDriver(),this);
+        }
+
+        @FindBy(xpath = "//a[ contains( . , 'Refrigeration' )]")
+        private WebElement CommercialRefrigerationLink;
+
+
+        public void clickCommercialRefrigerationLink(){
+            CommercialRefrigerationLink.click();
+        }
+
+    }
