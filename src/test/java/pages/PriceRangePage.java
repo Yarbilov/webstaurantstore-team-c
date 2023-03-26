f63d903a6a1c6700f6d445b4c930efc352ea1502
Dilnoza Akhrarova <akhrarovad@gmail.com>
2023-03-26 16:01:38 -0400

    price-range

--- src/test/java/pages/PriceRangePage.java
+++ src/test/java/pages/PriceRangePage.java
@@ -1,69 +1,148 @@
 package pages;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
+import utils.SeleniumUtils;
 
-    public class PriceRangePage {
+import java.util.List;
 
-        public PriceRangePage(){
+public class PriceRangePage {
 
-
-            PageFactory.initElements(Driver.getDriver(),this);//initialize everything that has @FindBy annotation
-        }
-
+    public PriceRangePage() {
 
 
-
-
-        @FindBy(name="searchval")
-        private WebElement searchBox;
-
-        @FindBy(xpath="//*[@id=\"categoriesBox\"]/div[2]/a/div/div/img")
-        private WebElement chefKnives;
-
-        @FindBy(xpath="//*[@id=\"panel-1\"]/a[2]/div[2]/div")
-        private   WebElement japaneseSteelKnives;
-
-        @FindBy( id="sort_options")
-        private WebElement sortOption;
-
-        @FindBy(xpath="//*[@id=\"sort_options\"]/optgroup[1]/option[1]")
-        private WebElement sortOptionByPriceLowToHigh;
-
-        @FindBy(xpath="//*[@id=\"sort_options\"]/optgroup[1]/option[2]")
-        private WebElement sortOptionByPriceHighToLow;
-
-        public void clickOnSearchBox(){
-            searchBox.click();
-        }
-
-        public void clickOnSubmit(){
-            searchBox.submit();
-        }
-
-        public void sendKeysToSearchBox(String itemName){
-
-            searchBox.sendKeys(itemName);
-        }
-
-
-        public void clickOnChefKnives(){
-            chefKnives.click();
-
-        }
-
-        public void clickOnJapaneseSteelKnives(){
-            japaneseSteelKnives.click();
-        }
-        public  void  clickOnSortOption(){
-            sortOption.click();
-        }
-        public void clickOnSortOptionByPriceLowToHigh(){
-            sortOption.click();
-        }
-        public void clickOnSortOptionByPriceHighToLow(){
-            sortOption.click();
+        PageFactory.initElements(Driver.getDriver(), this);//initialize everything that has @FindBy annotation
     }
 
+
+    @FindBy(name = "searchval")
+    private WebElement searchBox;
+
+    @FindBy(xpath = "//*[@id=\"categoriesBox\"]/div[2]/a/div/div/img")
+    private WebElement chefKnives;
+
+    @FindBy(xpath = "//*[@id=\"panel-1\"]/a[2]/div[2]/div")
+    private WebElement japaneseSteelKnives;
+
+    @FindBy(id = "sort_options")
+    private WebElement sortOption;
+
+    @FindBy(xpath = "//*[@id=\"sort_options\"]/optgroup[1]/option[1]")
+    private WebElement sortOptionByPriceLowToHigh;
+
+    @FindBy(xpath = "//*[@id=\"sort_options\"]/optgroup[1]/option[2]")
+    private WebElement sortOptionByPriceHighToLow;
+
+
+    @FindBy(xpath = "//*[@id=\"sort_options\"]/optgroup[2]/option[3]")
+    private WebElement sortOptionByRatingLowToHigh;
+
+    @FindBy(xpath = "//*[@id=\"sort_options\"]/optgroup[2]/option[4]")
+    private WebElement sortOptionByRatingHighToLow;
+
+    @FindBy(xpath = "//*[@id=\"sort_options\"]/optgroup[2]/option[5]")
+    private WebElement sortOptionByNewest;
+
+    @FindBy(xpath = "//*[@id=\"sort_options\"]/optgroup[2]/option[6]")
+    private WebElement sortOptionByOldest;
+
+    @FindBy(xpath = "//*[@id=\"ProductBoxContainer\"]/div[1]/a/div/img[2]")
+    private List<WebElement> list;
+
+
+    @FindBy(xpath = "//*[@id=\"ProductBoxContainer\"]/div[1]/a/div/img[2]")
+    private WebElement chooseItem;
+
+    @FindBy(xpath = "//*[@id=\"ProductBoxContainer\"]/div[4]/form/div/div/input[2]")
+    private WebElement addToCart;
+
+    @FindBy(xpath = "//*[@id=\"watnotif-wrapper\"]/div/p/div[2]/div[2]/a[2]")
+    private WebElement OnCart;
+
+    @FindBy(xpath = "//*[@id=\"subject\"]/div[2]/div/a[2]")
+    private WebElement checkout;
+
+
+
+
+
+
+    public void clickOnSearchBox() {
+        searchBox.click();
+    }
+
+    public void clickOnSubmit() {
+        searchBox.submit();
+    }
+
+    public void sendKeysToSearchBox(String itemName) {
+
+        searchBox.sendKeys(itemName);
+    }
+
+
+    public void clickOnChefKnives() {
+        chefKnives.click();
+
+    }
+
+    public void clickOnJapaneseSteelKnives() {
+        japaneseSteelKnives.click();
+    }
+
+    public void clickOnSortOption() {
+        sortOption.click();
+    }
+
+    public void clickOnSortOptionByPriceLowToHigh() {
+        sortOption.click();
+    }
+
+    public void clickOnSortOptionByPriceHighToLow() {
+        sortOption.click();
+
+    }
+
+    public void clickOnSortOptionRatingLowToHigh() {
+        sortOption.click();
+    }
+
+    public void clickOnSortOptionRatingHighToLow() {
+        sortOption.click();
+    }
+
+    public void clickOnSortOptionByNewest() {
+        sortOption.click();
+    }
+
+    public void clickOnSortOptionByOldest() {
+        sortOption.click();
+    }
+
+    public List<WebElement> getList(){
+
+        return list;
+    }
+
+    public void clickOnChooseItem() {
+        sortOption.click();
+    }
+
+    public void clickOnAddToCartChosenItem() {
+        addToCart.click();
+    }
+
+    public void clickOnCart() {
+        OnCart.click();
+    }
+
+    public void clickOnOpenCheckoutPage(){
+        checkout.click();
+    }
+
+
 }
+
+
--- src/test/java/tests/PriceRangeTest.java
+++ src/test/java/tests/PriceRangeTest.java
@@ -1,9 +1,14 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.PriceRangePage;
 import utils.Driver;
+
+import static org.testng.Assert.assertEquals;
+
+import utils.SeleniumUtils;
 
 public class PriceRangeTest extends TestBase {
 
@@ -12,7 +17,7 @@ public class PriceRangeTest extends TestBase {
         new PriceRangePage().clickOnSearchBox();
         new PriceRangePage().sendKeysToSearchBox("chef knives");
         new PriceRangePage().clickOnSubmit();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives - WebstaurantStore");
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives - WebstaurantStore");
 
 
     }
@@ -23,7 +28,7 @@ public class PriceRangeTest extends TestBase {
         new PriceRangePage().sendKeysToSearchBox("chef knives");
         new PriceRangePage().clickOnSubmit();
         new PriceRangePage().clickOnChefKnives();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
     }
 
     @Test
@@ -33,24 +38,22 @@ public class PriceRangeTest extends TestBase {
         new PriceRangePage().clickOnSubmit();
         new PriceRangePage().clickOnChefKnives();
         new PriceRangePage().clickOnJapaneseSteelKnives();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
     }
 
     @Test
-
-    public void testSortOption(){
+    public void testSortOption() {
         new PriceRangePage().clickOnSearchBox();
         new PriceRangePage().sendKeysToSearchBox("chef knives");
         new PriceRangePage().clickOnSubmit();
         new PriceRangePage().clickOnChefKnives();
         new PriceRangePage().clickOnJapaneseSteelKnives();
         new PriceRangePage().clickOnSortOption();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
     }
 
     @Test
-
-    public void testSortOptionByPriceLowToHigh(){
+    public void testSortOptionByPriceLowToHigh() {
         new PriceRangePage().clickOnSearchBox();
         new PriceRangePage().sendKeysToSearchBox("chef knives");
         new PriceRangePage().clickOnSubmit();
@@ -58,12 +61,12 @@ public class PriceRangeTest extends TestBase {
         new PriceRangePage().clickOnJapaneseSteelKnives();
         new PriceRangePage().clickOnSortOption();
         new PriceRangePage().clickOnSortOptionByPriceLowToHigh();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
     }
 
     @Test
 
-    public void testSortOptionByPriceHighToLow(){
+    public void testSortOptionByPriceHighToLow() {
         new PriceRangePage().clickOnSearchBox();
         new PriceRangePage().sendKeysToSearchBox("chef knives");
         new PriceRangePage().clickOnSubmit();
@@ -71,9 +74,129 @@ public class PriceRangeTest extends TestBase {
         new PriceRangePage().clickOnJapaneseSteelKnives();
         new PriceRangePage().clickOnSortOption();
         new PriceRangePage().clickOnSortOptionByPriceHighToLow();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
     }
 
+    @Test
+
+    public void testSortOptionByRatingLowToHigh() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @Test
+
+    public void testSortOptionByRatingHighToLow() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @Test
+
+    public void testSortOptionByNewest() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @Test
+
+    public void testSortOptionByOldest() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @Test
+
+    public void testChooseItem() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        new PriceRangePage().clickOnSortOptionByPriceLowToHigh();
+        new PriceRangePage().clickOnChooseItem();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @DataProvider(name = "data-provider")
+
+    public Object[][] dataProviderMethod() {
+        return new Object[][]{
+                {"chef knives"},
+                {"chef knife"},
+                {"chef knife set"}
+        };
+
+    }
+
+    @Test
+
+    public void testAddToCartChosenItem() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        new PriceRangePage().clickOnSortOptionByPriceLowToHigh();
+        new PriceRangePage().clickOnChooseItem();
+        new PriceRangePage().clickOnAddToCartChosenItem();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @Test
+
+    public void testClickOnCart() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        new PriceRangePage().clickOnSortOptionByPriceLowToHigh();
+        new PriceRangePage().clickOnChooseItem();
+        new PriceRangePage().clickOnAddToCartChosenItem();
+        assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+    }
+
+    @Test
+
+    public void testopenCheckoutPage() {
+        new PriceRangePage().clickOnSearchBox();
+        new PriceRangePage().sendKeysToSearchBox("chef knives");
+        new PriceRangePage().clickOnSubmit();
+        new PriceRangePage().clickOnChefKnives();
+        new PriceRangePage().clickOnJapaneseSteelKnives();
+        new PriceRangePage().clickOnSortOption();
+        new PriceRangePage().clickOnSortOptionByPriceLowToHigh();
+        new PriceRangePage().clickOnChooseItem();
+        new PriceRangePage().clickOnAddToCartChosenItem();
+        new PriceRangePage().clickOnCart();
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Chef Knives: 200+ Professional Chef Knives at Low Prices!");
+
+
+    }
 
 
 }
